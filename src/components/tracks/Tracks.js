588a9f54d4247597aec757844a9e2eb330a1eb5d
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-06-11 17:17:11 +0530

    Refactor: refactored files

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,14 +1,24 @@
 import React, { useContext } from "react";
 import LyricsContext from "../../lyrics-context";
 import Spinner from "../layout/Spinner";
+import Track from "../tracks/Track";
 
 const Tracks = () => {
-  const { track_list } = useContext(LyricsContext);
+  const { track_list, heading } = useContext(LyricsContext);
 
   if (track_list === undefined || track_list.length === 0) {
     return <Spinner />;
   } else {
-    return <h1>Tracks loaded</h1>;
+    return (
+      <>
+        <h3 className="text-center mb-4">{heading}</h3>
+        <div className="row">
+          {track_list.map((item) => (
+            <Track key={item.track.track_id} track={item.track} />
+          ))}
+        </div>
+      </>
+    );
   }
 };
 
